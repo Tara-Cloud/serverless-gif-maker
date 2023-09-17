0b0b099ca99301704b40687f14612d80ef2d635e
Tara S <tara.schoenherr@gmail.com>
2023-09-16 21:20:05 -0600

    remove broken heading from gif card

--- web-ui/src/components/GifCard.tsx
+++ web-ui/src/components/GifCard.tsx
@@ -14,7 +14,6 @@ const GifCard: React.FC<GifCardProps> = ({ gifURL, gifKey }) => {
     <div className="card card-compact glass">
       <img src={gifURL} alt={gifKey} className="gif-item" loading="lazy" />
       <div className="card-body items-center text-center">
-        <h2 className="card-title">{gifKey}</h2>
         <div style={{ display: "flex", gap: "10px" }}>
           <TrashIcon s3_key={gifKey}></TrashIcon>
           <FaveHeart s3_key={gifKey}></FaveHeart>
--- web-ui/src/services/api-client.ts
+++ web-ui/src/services/api-client.ts
@@ -1,5 +1,5 @@
 import axios, {CanceledError} from "axios";
 
 export default axios.create({
-    baseURL: "https://gal744o7q9.execute-api.us-east-1.amazonaws.com/prod",
+    baseURL: "https://eaqx59qktb.execute-api.us-east-1.amazonaws.com/prod/",
 })
